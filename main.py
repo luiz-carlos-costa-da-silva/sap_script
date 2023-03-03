2d8b9b80b5919a0175df4fc133dda9bdd8b74f89
luiz-carlos-costa-da-silva <102918661+luiz-carlos-costa-da-silva@users.noreply.github.com>
2023-03-03 18:59:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+'''
+Autor: Luiz Carlos Costa da Silva
+Data da criação: 01/02/2023
+Objetivo do projeto:
+Gerar relatório logístico diário e enviar a diretoria e a gerencia.
+'''
+
 from sap.script import Sap_Script
 
 sap_script = Sap_Script()
